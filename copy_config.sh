4d47c9dec4cd8ab56be599b83f7cd5875a19ea29
legelf36 <krfleg2@accesscomm.ca>
2024-09-10 15:55:40 -0600

    update config

--- copy_config.sh
+++ copy_config.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-cp -f ~/.bashrc ~/.scripts/.config/
-cp -f ~/.bash_aliases ~/.scripts/.config/
-cp -f ~/.vimrc ~/.scripts/.config/
-cp -rf ~/.config/ranger/ ~/.scripts/.config/
-cp -f ~/.config/conky/*Redux* ~/.scripts/.config/conky/
-cp -rf ~/.config/fastfetch/ ~/.scripts/.config/
-cp -rf ~/.config/picom/ ~/.scripts/.config/
-cp -rf ~/.config/polybar/ ~/.scripts/.config/
-cp -rf ~/.config/rofi/ ~/.scripts/.config/
-cp -rf ~/.config/terminator/ ~/.scripts/.config/
-cp -rf ~/.config/thefuck/ ~/.scripts/.config/
-cp -rf ~/.config/i3/config/ ~/.scripts/.config/
-cp -rf ~/.config/i3blocks/ ~/.scripts/.config/
-cp -rf ~/.config/i3status/ ~/.scripts/.config/
+cp -fv ~/.bashrc ~/.scripts/.config/
+cp -fv ~/.bash_aliases ~/.scripts/.config/
+cp -fv ~/.vimrc ~/.scripts/.config/
+cp -rfv ~/.config/ranger/ ~/.scripts/.config/
+cp -fv ~/.config/conky/*Redux* ~/.scripts/.config/conky/
+cp -rfv ~/.config/fastfetch/ ~/.scripts/.config/
+cp -rfv ~/.config/picom/ ~/.scripts/.config/
+cp -rfv ~/.config/polybar/ ~/.scripts/.config/
+cp -rfv ~/.config/rofi/ ~/.scripts/.config/
+cp -rfv ~/.config/terminator/ ~/.scripts/.config/
+cp -rfv ~/.config/thefuck/ ~/.scripts/.config/
+cp -rfv ~/.config/i3/config/ ~/.scripts/.config/
+cp -rfv ~/.config/i3blocks/ ~/.scripts/.config/
+cp -rfv ~/.config/i3status/ ~/.scripts/.config/
 
 git add -A
-git commit -m "update"
+git commit -m "update config"
 git push -u
 
