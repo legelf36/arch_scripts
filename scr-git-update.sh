12f101d767d25ec92244199aad374c172c8a6bc7
legelf36 <krfleg2@accesscomm.ca>
2025-10-07 21:08:09 -0600

    scripted update to scripts 2025-10-07

--- scr-git-update.sh
+++ scr-git-update.sh
@@ -3,17 +3,17 @@
 # This script is to update my 'arco-scripts' repository on github.
 
 # Push to the stack and move to the 'scripts' directory.
-# pushd /home/kim/.scripts
+pushd /home/kim/.scripts
 
+current_date=$(date +%Y-%m-%d)
 # Add any changes to 'master' branch.
 git add -A
 
 # Commit changes to the 'master' branch.
-git commit -m "update to scripts"
+git commit -m "scripted update to scripts $current_date"
 
 # Push the changes to the github repository.
 git push -u
 
 # Go to previous directory and remove '.scripts' from the stack
-# popd
-
+popd
