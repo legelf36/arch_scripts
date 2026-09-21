e4c3bd8681aedf5bb1e9823a682b873e81ee5a5c
legelf36 <krfleg2@accesscomm.ca>
2026-09-20 19:52:00 -0600

    scripted update to scripts 09/20/2026-19:52

--- bash/install/install_ivms4200.sh
+++ bash/install/install_ivms4200.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+
+# Extract iVMS4200-linux.gz to ~/.local/opt/
+# This will create a "Linux" directory there with the app files.
+# Below are the 32-bit libraires required.
 sudo pacman -S lib32-glibc lib32-gcc-libs lib32-libx11 lib32-libxext lib32-util-linux lib32-mesa lib32-glu
-y
-cd ~
-cd Downloads/Linux/
-lla
-LD_LIBRARY_PATH=./PlugIns ./iVMS-4200
+cd ~/.local/opt/iVMS-4200               # change to app directory.
+# Run app with library path set and send errors and messages to  file.
 setsid env LD_LIBRARY_PATH=./PlugIns ./iVMS-4200 > /tmp/ivms.log 2>&1 &
 
