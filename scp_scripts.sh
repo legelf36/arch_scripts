2e6146a576a83b81461e766adc9136310ae3e23d
Kim Flegel <krfleg2@accesscomm.ca>
2026-09-23 08:31:08 -0600

    scripted update to scripts 09/23/2026-08:31

--- scp_scripts.sh
+++
@@ -1,7 +0,0 @@
-# Syntax for "scp"
-#
-# copy file to host
-# scp SourceFile user@host:directory/TargetFile
-#
-# copy file from host and copying folder from host
-# scp -r user@host:directory/SourceFolder TargetFolder
